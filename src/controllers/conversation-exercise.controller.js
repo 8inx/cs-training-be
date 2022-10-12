20fe3703491d55eddf211a7cc227b7f5986b8ea9
8inx <acebo.dale@gmail.com>
2022-10-12 11:58:14 +0800

    Add ConversationExercise findBySessionId

--- src/controllers/conversation-exercise.controller.js
+++ src/controllers/conversation-exercise.controller.js
@@ -1,10 +1,20 @@
-const { bulkInsertExercise } = require('@services/conversation-exercise.service');
+const { bulkInsertExercise, findBySessionId } = require('@services/conversation-exercise.service');
 
 export const bulkInsertExerciseHandler = async (req, res, next) => {
   try {
     const input = req.body;
     const results = await bulkInsertExercise(input);
     res.status(200).json({ data: results, message: 'bulk insert exercise' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const findBySessionIdHandler = async (req, res, next) => {
+  try {
+    const sessionId = req.params.sessionId;
+    const conversationExercise = await findBySessionId(sessionId);
+    res.status(200).json({ data: conversationExercise, message: 'find conversation exercise' });
   } catch (error) {
     next(error);
   }
