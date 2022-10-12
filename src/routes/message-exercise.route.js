ceaaa6481c1c6ae6ae2d19a43fac281b365582d2
8inx <acebo.dale@gmail.com>
2022-10-12 18:56:17 +0800

    Add message-exercise

---
+++ src/routes/message-exercise.route.js
@@ -0,0 +1,93 @@
+import {
+  findByIdHandler,
+  findBySegmentHandler,
+  findBySessionIdHandler,
+} from '@controllers/message-conversation.controller';
+import { Router } from 'express';
+
+const route = Router();
+
+/**
+ * @openapi
+ * '/message-exercise/id/{id}':
+ *  get:
+ *    tags:
+ *      - message-exercise
+ *    summary: Find conversation exercise by id
+ *    parameters:
+ *    - name: id
+ *      in: path
+ *      type: string
+ *      description: message id
+ *      required: true
+ *    responses:
+ *      200:
+ *        description: 'Success'
+ *      400:
+ *        description: 'Bad Request'
+ *      404:
+ *        description: 'Not Found'
+ *      500:
+ *        description: 'Server Error'
+ */
+
+route.get('/id/:id', findByIdHandler);
+
+/**
+ * @openapi
+ * '/message-exercise/session/{sessionId}':
+ *  get:
+ *    tags:
+ *      - message-exercise
+ *    summary: Find message exercise by session id
+ *    parameters:
+ *    - name: sessionId
+ *      in: path
+ *      type: string
+ *      description: message session id
+ *      required: true
+ *    responses:
+ *      200:
+ *        description: 'Success'
+ *      400:
+ *        description: 'Bad Request'
+ *      404:
+ *        description: 'Not Found'
+ *      500:
+ *        description: 'Server Error'
+ */
+
+route.get('/session/:sessionId', findBySessionIdHandler);
+
+/**
+ * @openapi
+ * '/message-exercise/session/{sessionId}/segment/{segmentId}':
+ *  get:
+ *    tags:
+ *      - message-exercise
+ *    summary: Find message exercise by segment
+ *    parameters:
+ *    - name: sessionId
+ *      in: path
+ *      type: string
+ *      description: message session id
+ *      required: true
+ *    - name: segmentId
+ *      in: path
+ *      type: string
+ *      description: message segment id
+ *      required: true
+ *    responses:
+ *      200:
+ *        description: 'Success'
+ *      400:
+ *        description: 'Bad Request'
+ *      404:
+ *        description: 'Not Found'
+ *      500:
+ *        description: 'Server Error'
+ */
+
+route.get('/session/:sessionId/segment/:segmentId', findBySegmentHandler);
+
+export default route;
